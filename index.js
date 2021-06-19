333cc85d6ab665f0477fc3bb37147867e5259533
AL-HUSSEIN ABUBAKAR <al-hussein.abubakar@amalitech.org>
2021-06-19 17:09:58 +0000

    modified js files

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ document.querySelectorAll("img")[1].setAttribute("src", randomImageSource2);
 
 
 if (randomNumber1 > randomNumber2) {
-  document.querySelector("h1").innerHTML = "🚩 Play 1 Wins!";
+  document.querySelector("h1").innerHTML = " Play 1 Wins! ✔";
 }
 else if (randomNumber2 > randomNumber1) {
-  document.querySelector("h1").innerHTML = "Player 2 Wins! 🚩";
+  document.querySelector("h1").innerHTML = "Player 2 Wins! ✔";
 }
 else {
   document.querySelector("h1").innerHTML = "Draw!";
